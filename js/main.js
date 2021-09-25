450a2ea68104a430a9add36fa80019adcf77737c
Людмила <semenyuk0308@gmail.com>
2021-09-25 15:40:56 +0300

    Добавила примеры функций

--- js/main.js
+++ js/main.js
@@ -3,24 +3,24 @@ const getRandomInt = function(min,max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
-console.log(getRandomInt(1,100));//Максимум и минимум включаются
+(getRandomInt(1,100));//Максимум и минимум включаются
 
 const getRandomIntNew = function(min,max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
-}
+};
 
-console.log(getRandomIntNew(10,50)) //Максимум не включается, минимум включается
+getRandomIntNew(10,50); //Максимум не включается, минимум включается
 
 //Функция для проверки максимальной длины строки
 const checkLength = function (currentString, maxLength) {
 
   if (currentString.length < maxLength) {
     return true;
-  } else {return false};
+  }; else {return false}
 }
 
 console.log(checkLength('Привет', 140));
