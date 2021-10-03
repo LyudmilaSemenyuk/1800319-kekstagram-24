00efaf601f5fb1025c2dc219d5de00e1e73a5c6f
Людмила <semenyuk0308@gmail.com>
2021-10-04 00:09:25 +0300

    Добавила функции для создания массива из 25 сгенерированных объектов

--- js/main.js
+++ js/main.js
@@ -19,3 +19,59 @@ const checkLength = function (currentString, maxLength) {
 };
 
 checkLength('Привет', 140);
+
+// module4-task1
+
+const PHOTO_DESCRIPTION = [
+  'На море',
+  'На работе',
+  'На прогулке',
+  'Отпуск продолжается',
+  'В ресторане',
+  'Свадьба друга',
+  'Мой завтрак',
+  'Мой кот',
+  'На рыбалке',
+  'Мои племянники',
+];
+
+const COMMENTS_MESSAGE = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const COMMENTS_NAME = [
+  'Михаил',
+  'Татьяна',
+  'Жанна',
+  'Машенька',
+  'Катюша',
+  'Алиса',
+];
+
+const createPost = (index) => ({
+  id: index,
+  url: `photos/${index}.jpg`,
+  description: PHOTO_DESCRIPTION[getRandomInt(0, PHOTO_DESCRIPTION.length - 1)],
+  likes: getRandomInt(15, 200),
+  comments: [
+    {
+      id: getRandomInt(1, 25),
+      avatar: `img/avatar-${getRandomInt(1, 6)}.svg`,
+      message: COMMENTS_MESSAGE[getRandomInt(0, COMMENTS_MESSAGE.length - 1)],
+      name: COMMENTS_NAME[getRandomInt(0, COMMENTS_MESSAGE.length - 1)],
+    },
+  ],
+});
+
+const POSTS = [];
+
+for (let index = 0; index < 25; index++){
+  POSTS.push(createPost(index));
+}
+
+
