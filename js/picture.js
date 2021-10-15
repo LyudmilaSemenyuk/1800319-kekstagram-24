cb881ffe2405a51e8905da2f869c7bf81adc90e2
Людмила <semenyuk0308@gmail.com>
2021-10-15 09:44:01 +0300

    Создала DOM-элементы, соответствующие фотографиям

--- js/picture.js
+++ js/picture.js
@@ -1,7 +1,7 @@
 import {PICTURES} from './data.js';
 const pictureContainer = document.querySelector('.pictures');
+const similarListFragment = document.createDocumentFragment();
 const newPictureTemplate = document.querySelector('#picture').content;
-// const listOfPictures = createPicture();
 
 const resize = function () {
   PICTURES.forEach((picture) => {
@@ -9,8 +9,9 @@ const resize = function () {
     newPicture.querySelector('.picture__img').src = picture.url;
     newPicture.querySelector('.picture__likes').textContent = picture.likes;
     newPicture.querySelector('.picture__comments').textContent = picture.comments.length;
-    pictureContainer.appendChild(newPicture);
+    similarListFragment.appendChild(newPicture);
   });
+  pictureContainer.appendChild(similarListFragment);
 };
 
 
