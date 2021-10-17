13ff660e9511cebbbe1e1f0d43148148a8ea64eb
Людмила <semenyuk0308@gmail.com>
2021-10-17 19:54:19 +0300

    Реализовала сценарий просмотра фото в полноразмерном режиме

--- js/picture.js
+++ js/picture.js
@@ -1,18 +1,16 @@
 import {PICTURES} from './data.js';
+import './fullscreen-picture.js';
+import { addingEventListener } from './fullscreen-picture.js';
 const pictureContainer = document.querySelector('.pictures');
 const similarListFragment = document.createDocumentFragment();
 const newPictureTemplate = document.querySelector('#picture').content;
 
-const resize = function () {
-  PICTURES.forEach((picture) => {
-    const newPicture = newPictureTemplate.cloneNode(true);
-    newPicture.querySelector('.picture__img').src = picture.url;
-    newPicture.querySelector('.picture__likes').textContent = picture.likes;
-    newPicture.querySelector('.picture__comments').textContent = picture.comments.length;
-    similarListFragment.appendChild(newPicture);
-  });
-  pictureContainer.appendChild(similarListFragment);
-};
-
-
-export {resize};
+PICTURES.forEach((picture) => {
+  let newPicture = newPictureTemplate.cloneNode(true);
+  newPicture.querySelector('.picture__img').src = picture.url;
+  newPicture.querySelector('.picture__likes').textContent = picture.likes;
+  newPicture.querySelector('.picture__comments').textContent = picture.comments.length;
+  newPicture = addingEventListener(newPicture, picture);
+  similarListFragment.appendChild(newPicture);
+});
+pictureContainer.appendChild(similarListFragment);
---
+++ js/popup.js
@@ -0,0 +1,17 @@
+const pictureCancelButton = document.querySelector('#picture-cancel');
+const bigPictureSection = document.querySelector('.big-picture');
+
+pictureCancelButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  bigPictureSection.classList.add('hidden');
+  document.querySelector('body').classList.remove('modal-open');
+});
+
+window.addEventListener('keydown', (evt) => {
+  evt.preventDefault();
+  if(evt.key === 'Escape') {
+    bigPictureSection.classList.add('hidden');
+    document.querySelector('body').classList.remove('modal-open');
+  }
+});
+
